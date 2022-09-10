5de546fa17713dc947822defa7540befa1428ceb
willgee9531 <willgee9531@gmail.com>
2022-09-09 21:42:13 -0700

    C is an awesome Language

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - Print numbers from 00 to 99
- *
- *Return: Always 0 (Success)
+ * * main - Print combinations of two digit numbers
+ * *
+ * * Return: Always 0 (Success)
  *
  */
 int main(void)
@@ -13,15 +13,15 @@ int main(void)
 
 	for (tens = 0; tens <= 9; tens++)
 	{
-	for (ones = 0; ones <= 9; ones++)
-	{
-	putchar(tens + '0');
-	putchar(ones + '0');
-	if (!(tens == 9 && ones == 9))
-	{
-	putchar(',');
-	putchar(' ');
-	}
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+		putchar(tens + '0');
+		putchar(ones + '0');
+		if (tens < 8)
+		{
+		putchar(',');
+		putchar(' ');
+		}
 	}
 	}
 	putchar('\n');
