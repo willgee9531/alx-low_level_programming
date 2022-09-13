f2c895ebad46b7774f606c417f46c1e1067b32a0
willgee9531 <willgee9531@gmail.com>
2022-09-13 14:32:13 -0700

    printed a check for an phabetic character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c:The character in letters
+ * Return: 1 and  0 depending the condition
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
