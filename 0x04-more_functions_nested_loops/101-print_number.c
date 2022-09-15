15c2cc83434b882cc4887dd1df5413f0dd302817
willgee9531 <willgee9531@gmail.com>
2022-09-15 15:42:01 -0700

    more functions and nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,23 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * print_number - prints a number
- * @n: Input number
+ * * print_number - Prints a number
+ * * @n: The number to print
  */
 
 void print_number(int n)
 {
-	long len, res, i, temp, expo;
+	unsigned int num = n;
 
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
-	{
-		res *= -1;
-		_putchar('-');
-	}
-
-/**/
-	temp = res;
-	while (temp >= 10)
-	{
-		len++;
-		temp /= 10;
-	}
-
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+		if (n < 0)
+		{
+			putchar('-');
+			num = -num;
+		}
+		if (num > 9)
+		{
+			print_number(num / 10);
+		}
+		putchar(num % 10 + '0');
 }
