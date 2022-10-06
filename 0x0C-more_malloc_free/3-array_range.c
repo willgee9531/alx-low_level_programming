0c6b58c9b49ffc5f609219d781307d7ab198f400
willgee9531 <willgee9531@gmail.com>
2022-10-06 16:06:49 -0700

    more malloc tasks done!

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,25 +3,25 @@
 
 /**
  * array_range - creates an array of integers
- * @min: first integer number
- * @max: last integer number
+ * @min: minimum integer in the array
+ * @max: maximum integer to an array
  *
  * Return: pointer to newly allocated memory or NULL if fails
  */
 int *array_range(int min, int max)
 {
-	int i, l;
-	int *a;
+	int i, size;
+	int *p;
 
 	if (min > max)
 		return (NULL);
-	l = max - min + 1;
-	a = malloc(sizeof(int) * 1);
-	if (a == NULL)
+	size = (max - min) + 1;
+	p = malloc(sizeof(*p) * size);
+	if (p == NULL)
 		return (NULL);
-	for (i = 0; i < 1; min++)
+	for (i = 0; i < size && min <= max; i++, min++)
 	{
-		a[i] = min;
+		*(p + i) = min;
 	}
-	return (a);
+	return (p);
 }
